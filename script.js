083864e618cd44da5d088ad86df6e613c058fb51
Your Name <23096yb@hvhs.school.nz>
2026-06-15 11:05:03 +1200

    uid dosnt workkk

---
+++ script.js
@@ -0,0 +1,30 @@
+const HTML_OUTPUT = document.getElementById('statusMessage')
+console.log("runing website")
+
+function writeForm() {
+    // Get the form data
+    let fruitPeople = GLOBAL_user.uid
+    const favoriteFruit = document.getElementById("favoriteFruit").value;
+    const name = document.getElementById("name").value;
+    const fruitQuantity = document.getElementById("fruitQuantity").value;
+    console.log("fruitpplworks")
+
+    firebase.database().ref('/' + fruitPeople).set(
+        {
+            name: name,
+            fruit: favoriteFruit,
+            fruitNumber: fruitQuantity,
+        }
+    )
+}
+function list() {
+    console.log("Running list()")
+    firebase.database().ref('/').set(
+        {
+            sector1: {
+                consumers: {
+                }
+            }
+        }
+    )
+}
